b18271502ff41cc1c514e50ad0448696d67acad9
JoshAltman <joshuaealtman@gmail.com>
2024-01-27 22:12:14 -0600

    added menus

--- Source/MetalGearSloth/MetalGearSloth.Build.cs
+++ Source/MetalGearSloth/MetalGearSloth.Build.cs
@@ -8,6 +8,6 @@ public class MetalGearSloth : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "AIModule", "UMG" });
 	}
 }
